a75a20bb71ce9b7d395c0937dcaa67fe0dc6aa21
Thomas Stassin <thomas.stassin@gsmproductions.com>
2016-04-18 21:51:43 +0200

    first exercice

---
+++ projects/lesson_4/Assets/Custom Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeMaterial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	Material solasSkull = (Material)Resources.Load("Solas skull");
+    GetComponent<Renderer>().material = solasSkull;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ projects/lesson_4/Assets/Custom Assets/Scripts/GenerateCapsule.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateCapsule : MonoBehaviour {
+
+    public Vector3 pinkPosition;
+	// Use this for initialization
+	void Start () {
+	GameObject pink = (GameObject)Resources.Load("pink");
+    Instantiate(pink, pinkPosition, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
